4f7515594856b3a3ab941e6091e89a5a12ecfb19
Ayush Tiwari <ayush.tiwari@team.cliffex.com>
2023-02-24 00:18:45 +0530

    add social media

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/iamayushtiwari?tab=repositories";
+const repoURL = "https://github.com/iamayushtiwari/myportfolio";
 
 ghpages.publish(
   pathname,
