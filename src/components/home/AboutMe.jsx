dab4b72032307a8c3e7061f0725a170f7b01ef24
Ayush Tiwari <ayush.tiwari@team.cliffex.com>
2023-03-02 13:24:51 +0530

    change font weight

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -49,7 +49,7 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
         </div>
         <div className={`col-lg-${showPic ? "7" : "12"}`}>
           <h2 className="display-4 mb-5 text-center">{heading}</h2>
-          <p className="lead text-center font-weight-bolder text-muted">{message}</p>
+          <p className="lead text-center font-weight-bold text-muted">{message}</p>
           {resume && (
             <p className="lead text-center">
               <a
